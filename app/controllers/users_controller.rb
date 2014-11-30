c93681665ded7a46718492da32dbcdde91517003
a kun <zhukun6150909@outlook.com>
2014-11-30 12:49:56 +0800

    push heroku

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all  
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:] = '添加成功'
+      redirect_to users
+    else
+      flash.now[:danger] = '添加失败'
+      render :new
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @uesr = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = '修改成功'
+      redirect_to users_path
+    else
+      flash.now[:danger] = '修改失败,请重试'
+      render :new
+    end
+  end
+
+  def destroy
+    User.destroy(User.find(params[:id])) if params[:id]
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+  end
+
+end
