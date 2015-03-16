a43323cb0057bc0e2ca816dd8245f01fc8b203fa
zhukun <zhukun@geekpark.net>
2015-03-16 22:19:07 +0800

    使用unicorn

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'settingslogic'
 gem 'handlebars_assets'
 
 
+gem 'unicorn'
+
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
---
+++ config/unicorn.rb
@@ -0,0 +1,29 @@
+root = '/root/datemanager/my-work'
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+listen 8080, :tcp_nopush => true
+
+stderr_path APP_PATH + "/log/unicorn.stderr.log"
+stdout_path APP_PATH + "/log/unicorn.stdout.log"
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
