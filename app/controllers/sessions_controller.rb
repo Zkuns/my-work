5d64796f4b1614373c33b9e26bfbdd760143479b
zhukun <zhukun@geekpark.net>
2015-03-19 14:37:23 +0800

    修bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,38 @@ class SessionsController < ApplicationController
       session[:id] = @user.id
       cookies[:remember_token] = @user.remember_token
       @com = @user.company
-      @user.admin? ? (redirect_to companies_path) : (redirect_to  company_items_path(@com))
+      if @user.admin?
+        @user = User.new(username: user_params[:username])
+        flash.now[:danger] = '身份错误'
+        render :new
+      else
+        redirect_to  company_items_path(@com)
+      end
+    else
+      @user = User.new(username: user_params[:username])
+      flash.now[:danger] = '账号密码错误，请联系管理员'
+      render :new
+    end
+  end
+
+  def admin_new
+    @user = User.new
+  end
+
+  def admin_create
+    @user = User.find_by(username: user_params[:username])
+    if @user && @user.authenticate(user_params[:password])
+      @user.update_attribute(:remember_token, SecureRandom.urlsafe_base64)
+      session[:id] = @user.id
+      cookies[:remember_token] = @user.remember_token
+      @com = @user.company
+      if @user.admin?
+        redirect_to companies_path
+      else
+        @user = User.new(username: user_params[:username])
+        flash.now[:danger] = '不是管理员'
+        render :new
+      end
     else
       @user = User.new(username: user_params[:username])
       flash.now[:danger] = '账号密码错误，请联系管理员'
